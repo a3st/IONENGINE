40017e7b4576bd058e77b5b0992bfb569f2f529a
Dmitriy Lukovenko <mludima23@gmail.com>
2022-05-29 01:21:13 +0800

    1

--- ui/render_interface.cpp
+++ ui/render_interface.cpp
@@ -1,7 +1,7 @@
 // Copyright © 2020-2022 Dmitriy Lukovenko. All rights reserved.
 
 #include <precompiled.h>
-#include <ui/render_inteface.h>
+#include <ui/render_interface.h>
 
 using namespace ionengine;
 using namespace ionengine::ui;
