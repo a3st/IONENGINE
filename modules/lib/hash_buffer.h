9419183ac8f6b958c99f016d474d10b9199a0321
Dmitriy Lukovenko <mludima23@gmail.com>
2022-06-26 14:55:01 +0800

    к

--- modules/lib/hash_buffer.h
+++ modules/lib/hash_buffer.h
@@ -6,60 +6,52 @@
 
 namespace ionengine::lib {
 
-template<class Type>
+template <class Type>
 class HashBuffer {
-public:
-
+ public:
     HashBuffer() = default;
 
-    HashBuffer(std::span<Type const> const data) {
-        _data.resize(data.size());
-        std::memcpy(_data.data(), data.data(), data.size_bytes());
-        _data_hash = XXHash64::hash(reinterpret_cast<void*>(_data.data()), _data.size() * sizeof(Type), 0);
+    HashBuffer(Type const* const data, size_t const size) {
+        _data = std::vector<Type>(size * sizeof(Type));
+        std::memcpy(_data.data(), data, size * sizeof(Type));
+        _hash = XXHash64::hash(data, size * sizeof(Type), 0);
     }
 
     HashBuffer(HashBuffer const& other) {
         _data = other._data;
-        _data_hash = other._data_hash;
+        _hash = other._data_hash;
     }
 
     HashBuffer(HashBuffer&& other) noexcept {
         _data = std::move(other._data);
-        _data_hash = other._data_hash;
+        _hash = std::move(other._data_hash);
     }
 
     HashBuffer& operator=(HashBuffer const& other) {
         _data = other._data;
-        _data_hash = other._data_hash;
+        _hash = other._data_hash;
         return *this;
     }
 
     HashBuffer& operator=(HashBuffer&& other) noexcept {
         _data = std::move(other._data);
-        _data_hash = other._data_hash;
+        _hash = std::move(other._data_hash);
         return *this;
     }
 
-    uint64_t hash() const { 
-        return _data_hash;
-    }
+    inline uint64_t hash() const { return _hash; }
 
-    Type const* data() const {
-        return _data.data();
-    }
+    inline Type const* data() const { return _data.data(); }
 
-    size_t size() const {
-        return _data.size();
-    }
+    inline size_t size() const { return _data.size(); }
 
-    std::span<Type const> to_span() {
+    inline std::span<Type const> to_span() {
         return std::span<Type const>(_data.data(), _data.size());
     }
 
-private:
-    
+ private:
     std::vector<Type> _data;
-    uint64_t _data_hash{0};
+    uint64_t _hash;
 };
 
-}
+}  // namespace ionengine::lib
