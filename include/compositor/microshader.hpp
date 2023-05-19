df2d5147da5256952a1e23d1e73843e8b78c844c
Dmitriy Lukovenko <mludima23@gmail.com>
2023-05-19 14:42:17 +0800

    1

--- include/compositor/microshader.hpp
+++ include/compositor/microshader.hpp
@@ -4,10 +4,29 @@ namespace ie {
 
 namespace compositor {
 
+enum class ShaderParameterType {
+    Float,
+    Float2,
+    Float3,
+    Float4,
+    Float2x2,
+    Float3x3,
+    Float4x4,
+    Boolean
+};
+
+struct ShaderParameter {
+    std::string name;
+    std::string description;
+    std::string semantic
+};
+
 class MicroShader {
 public:
 
     MicroShader(std::filesystem::path const& file_path);
+
+
 
 private:
 
