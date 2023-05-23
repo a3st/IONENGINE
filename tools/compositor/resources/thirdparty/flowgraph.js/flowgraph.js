2777b4f5d11da380a854e96020db7f90fd760afa
Dmitriy Lukovenko <mludima23@gmail.com>
2023-05-24 03:45:19 +0800

    1

--- tools/compositor/resources/thirdparty/flowgraph.js/flowgraph.js
+++ tools/compositor/resources/thirdparty/flowgraph.js/flowgraph.js
@@ -6,33 +6,56 @@ export default class FlowGraph {
         this.rootNode = rootNode;
         this.zoomScale = 1.0;
         this.selectedNode = null;
+        this.posNodeX = 0;
+        this.posNodeY = 0;
+        this.nodeIndex = 0;
+        this.nodes = { };
     }
 
     start() {
         $(this.rootNode).addClass('flowgraph-container');
         $(this.rootNode).append(`
             <div class="flowgraph-bg-container"></div>
+            <div class="flowgraph-canvas"></div>
         `);
 
         this.rootNode.addEventListener('mousewheel', this.zoom_event_handler.bind(this));
         this.rootNode.addEventListener('mousedown', this.drag_node_begin_handler.bind(this));
         this.rootNode.addEventListener('mouseup', this.drag_node_end_handler.bind(this));
         this.rootNode.addEventListener('mousemove', this.move_node_handler.bind(this));
+
+        $(this.rootNode).children('.flowgraph-canvas').css('transform', 'translate(0px, 0px)');
     }
 
     drag_node_begin_handler(e) {
         if($(e.target).closest('.flowgraph-io').length > 0) {
+
+            /*const target = $(e.target).closest('.flowgraph-io').get(0);
+
+            $(this.rootNode).append(`
+                    <svg height="800" width="800">
+                        <path d="M " stroke="black" fill="transparent"/>
+                    </svg>
+                `);
+            */
+
             return
         }
 
         this.selectedNode = $(e.target)
             .closest('.flowgraph-node-container')
             .get(0);
+
+        /*const posMatrix = $(this.selectedNode).css('transform').match(/-?[\d\.]+/g);
+        this.posNodeX = posMatrix[4];
+        this.posNodeY = posMatrix[5];*/
     }
 
     drag_node_end_handler(e) {
         if(this.selectedNode) {
             this.selectedNode = null;
+            this.posNodeX = 0;
+            this.posNodeY = 0;
         }
     }
 
@@ -51,12 +74,12 @@ export default class FlowGraph {
         } else {
             this.zoomScale -= 0.1;
         }
-        // console.log(this.zoomScale)
+        $(this.rootNode).children('.flowgraph-canvas').css('transform', `translate(0px, 0px) scale(${1.0 - this.zoomScale})`);
     }
 
     add_node(x, y, inputs, outputs, headerHTML) {
-        $(this.rootNode).append(`
-            <div class="flowgraph-node-container" style="width: 350px; height: 200px;"> 
+        $(this.rootNode).children('.flowgraph-canvas').append(`
+            <div id="node_${this.nodeIndex}" class="flowgraph-node-container" style="width: 350px; height: 200px;"> 
                 <div class="flowgraph-node-root">
                     <div class="flowgraph-node-header">
                         ${headerHTML}
@@ -94,6 +117,14 @@ export default class FlowGraph {
                     <div>
                 </div>
             </div>
-        `)
+        `);
+
+        $(`#node_${this.nodeIndex}`).css('transform', `translate(${x}px, ${y}px)`);
+
+        this.nodes[`node_${this.nodeIndex}`] = {
+            "position" : [x, y]
+        };
+
+        this.nodeIndex += 1;
     }
 }
