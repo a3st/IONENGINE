2cd73f282ccb972fba75fbe6300c69d2cede8b4a
Dmitriy Lukovenko <mludima23@gmail.com>
2024-10-06 07:27:21 +0800

    render pass

--- engine/renderer/renderer.cpp
+++ engine/renderer/renderer.cpp
@@ -5,16 +5,45 @@
 
 namespace ionengine
 {
-    Renderer::Renderer(rhi::Device& device) : device(&device)
+    Renderer::Renderer(rhi::Device& device, std::vector<core::ref_ptr<RenderPass>> const& renderPasses)
+        : device(&device), renderPasses(renderPasses)
     {
+        graphicsContext = device.createGraphicsContext();
+        copyContext = device.createCopyContext();
+
+        for (auto& renderPass : renderPasses)
+        {
+            renderPass->graphicsContext = graphicsContext.get();
+            renderPass->copyContext = copyContext.get();
+        }
     }
 
     auto Renderer::render() -> void
     {
+        graphicsContext->reset();
+
+        graphicsContext->setViewport(0, 0, outputWidth, outputHeight);
+        graphicsContext->setScissor(0, 0, outputWidth, outputHeight);
+
+        for (auto const& renderPass : renderPasses)
+        {
+            renderPass->render();
+        }
+
+        rhi::Future<rhi::Query> graphicsResult = graphicsContext->execute();
+
+        device->presentBackBuffer();
+
+        graphicsResult.wait();
     }
 
     auto createShader(shadersys::fx::ShaderEffectFile const& shaderEffect) -> core::ref_ptr<Shader>
     {
         return nullptr;
+    }
+
+    auto RendererBuilder::build(rhi::Device& device) -> core::ref_ptr<Renderer>
+    {
+        return core::make_ref<Renderer>(device, renderPasses);
     }
 } // namespace ionengine
--- engine/renderer/renderer.hpp
+++ engine/renderer/renderer.hpp
@@ -2,40 +2,60 @@
 
 #pragma once
 
+#include "core/handle.hpp"
 #include "rhi/rhi.hpp"
 #include "shader.hpp"
 #include "texture.hpp"
 
 namespace ionengine
 {
+    struct RenderPassTexture
+    {
+    };
+
+    class RenderPassCreateInfo
+    {
+      public:
+        RenderPassCreateInfo() = default;
+
+        auto createTexture(rhi::TextureFormat const format,
+                           rhi::TextureUsage const usage) -> core::handle<RenderPassTexture>;
+    };
+
+    class RenderPassStorage
+    {
+      public:
+        auto getTexture(core::handle<RenderPassTexture> const handle) -> core::ref_ptr<rhi::Texture>;
+
+        core::ref_ptr<rhi::Texture> backBuffer;
+    };
+
     class RenderPass : public core::ref_counted_object
     {
       public:
+        virtual ~RenderPass() = default;
+
         virtual auto render() -> void = 0;
+
+        rhi::GraphicsContext* graphicsContext;
+        rhi::CopyContext* copyContext;
+
+      protected:
+        RenderPassStorage* passStorage;
     };
 
-    class RendererBuilder
+    class Renderer : public core::ref_counted_object
     {
       public:
-        RendererBuilder();
-
-        template <typename Type = RenderPass>
-        auto addPass();
-
-      private:
-        std::vector<core::ref_ptr<RenderPass>> renderPasses;
-    };
-
-    class Renderer : core::ref_counted_object
-    {
-      public:
-        Renderer(rhi::Device& device);
+        Renderer(rhi::Device& device, std::vector<core::ref_ptr<RenderPass>> const& renderPasses);
 
         auto createShader(shadersys::fx::ShaderEffectFile const& shaderEffect) -> core::ref_ptr<Shader>;
 
         auto createTexture() -> core::ref_ptr<Texture>;
 
         auto render() -> void;
+
+        auto resize(uint32_t const width, uint32_t const height) -> void;
 
         /*
 
@@ -44,6 +64,33 @@ namespace ionengine
       private:
         rhi::Device* device;
 
+        core::ref_ptr<rhi::GraphicsContext> graphicsContext;
+        core::ref_ptr<rhi::CopyContext> copyContext;
+
+        std::vector<core::ref_ptr<RenderPass>> renderPasses;
         std::unordered_map<std::string, core::ref_ptr<Shader>> shaders;
+
+        uint32_t outputWidth;
+        uint32_t outputHeight;
+    };
+
+    class RendererBuilder
+    {
+      public:
+        RendererBuilder() = default;
+
+        template <typename Type = RenderPass>
+        auto addPass() -> RendererBuilder&
+        {
+            RenderPassCreateInfo passCreateInfo;
+            auto renderPass = core::make_ref<Type>(passCreateInfo);
+            renderPasses.emplace_back(std::move(renderPass));
+            return *this;
+        }
+
+        auto build(rhi::Device& device) -> core::ref_ptr<Renderer>;
+
+      private:
+        std::vector<core::ref_ptr<RenderPass>> renderPasses;
     };
 } // namespace ionengine
